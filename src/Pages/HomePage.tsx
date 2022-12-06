8299e3341aa41e5ce84b3e2872340976db95da56
Jose Felipe Terdes <josefelipe.terdes@cit.edu>
2022-12-06 21:35:15 +0800

    Modify HomePage.tsx

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import '../App.css';
 import Footer from '../components/garcia/garciaFooter';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
@@ -7,10 +7,10 @@ import ResponsiveAppBar from '../components/ResponsiveAppBar';
 function HomeApp() {
   return (
     <div className="App">
-        <ResponsiveAppBar/>
-        <h1>Reserve a Seat</h1>
-        <h4 style={{fontWeight:100}}>SakayTa Bus Reservation System is a system that reserves a bus and is a wonderful system with handsome</h4>
-        <Footer name="Victor Emmanuel F. Garcia" course="BSCS" section="F1"/>
+      <ResponsiveAppBar />
+      <h1>Reserve a Seat</h1>
+      <h4 style={{ fontWeight: 100 }}>SakayTa Bus Reservation System is a system that reserves a bus and is a wonderful system with handsome</h4>
+      <Footer name="Victor Emmanuel F. Garcia" course="BSCS" section="F1" />
     </div>
   );
 }
