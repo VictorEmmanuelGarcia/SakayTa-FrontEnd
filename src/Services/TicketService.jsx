3d882fc7733f0034d7619fa37a159eeaf2a0ac67
Jose Felipe Terdes <josefelipe.terdes@cit.edu>
2022-12-06 21:36:34 +0800

    Implement TicketService.jsx

---
+++ src/Services/TicketService.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const TICKET_BASES_URL = "http://localhost:8080/ticket/getAllTickets";
+
+class TicketService {
+    static getAllTickets() {
+        throw new Error('Method not implemented.');
+    }
+    getAllTickets() {
+        return axios.get(TICKET_BASES_URL);
+    }
+}
+
+export default TicketService;
