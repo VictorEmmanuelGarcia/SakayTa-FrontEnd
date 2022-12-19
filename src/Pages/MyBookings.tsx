e154532f49a6d614050e81fe5b39afb78b933c85
Andre Enriquez <andre.enriquez@cit.edu>
2022-12-20 01:33:54 +0800

    Add My Bookings

--- src/Pages/MyBookings.tsx
+++ src/Pages/MyBookings.tsx
@@ -4,46 +4,26 @@ import { Link } from 'react-router-dom';
 import '../reservation.css';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
 import Footer from '../components/garcia/garciaFooter';
+import Sidebar from '../components/sidebar';
+import Namebar from '../components/enriquez/namebar';
 
 function MyBookings(this: any) {
     return (
-        <div className="App">
+        <div className="Settings">
         <ResponsiveAppBar/>
-        <div className = "reservation__content" >
-                <div className = "reservation__cards">
-                <Stack direction="row" spacing={20}>
-                        <article className='reservation__card'>
-                            <h1>Main Information</h1>
-                            <h4>22 December 2022, Thursday</h4>
-                            <h4>1 person</h4>
-                            <h4>Airconditioned</h4>
-                            <h4>Librando</h4>
-                            <h4>31 seats</h4>
-                            <h4>Php 180</h4>
-                            <h1>Schedule</h1>
-                            <h4>6:00 AM South Bus Terminal</h4>
-                            <h5>6:30 AM Pardo</h5>
-                            <h5>7:00 AM Naga</h5>
-                            <h5>7:30 AM Carcar</h5>
-                            <h4>8:00 AM Dalaguete</h4>
-                            <h4>Travel Time: 2 hours</h4>
-                        </article>
-                        <div>
-                            <h1 style={{display:'flex', marginTop: "65px"}}>Payment Method</h1>
-                            <Link to='/reservation'>
-                            <Stack direction="column" spacing={2}>
-                            <Button variant="outlined"  size="large" sx={{borderColor:'#CA7715', color: 'black', bottom: 30, top: 20, width: 500, borderRadius:2, fontWeight:"600", ":hover": {backgroundColor: '#F79110', borderColor: '#F79110', color: 'white'}}}>GCash</Button>
-                            <Button variant="outlined" size="large" sx={{borderColor:'#CA7715', color: 'black', bottom: 30, top: 20, borderRadius:2, fontWeight:"600", ":hover": {backgroundColor: '#F79110', borderColor: '#F79110', color: 'white'}}}>Maya</Button>
-                            <Button variant="outlined" size="large" sx={{borderColor:'#CA7715', color: 'black', bottom: 50, top: 20, borderRadius:2, fontWeight:"600", ":hover": {backgroundColor: '#F79110', borderColor: '#F79110', color: 'white'}}}>Pay on the go</Button>
-                            </Stack>
-                            </Link>
-                        </div>
-                    </Stack>
-                </div>
-        </div>
-        <div style={{marginTop: "-30px"}}>
-        <Footer name="Andre G. Enriquez" course="BSCS" section="F2"/>
-        </div>
+        <Sidebar/>
+        <Namebar/>
+            <div className="Settings-bar" style={{display: "flex"}}>
+                <Stack direction="column" spacing={3}>
+                <h1 style={{color: 'black', width: 1200, bottom: 30, top: 30, right: 100,fontWeight:"700"}}>My Bookings</h1>
+                <h1 style={{color: 'black', width: 2000, bottom: 30, top: 30, right: 50, fontWeight:"649.5", fontSize: "25px"}}>You have no bookings yet.</h1>
+                <Link to='/schedulelist'>
+                    <Button variant="contained" style={{backgroundColor:'black', bottom: 50, top: 30, left: 940, borderRadius:2, fontWeight:"750", width: "150px", height: "60px"}}>Reserve Now</Button>
+                </Link>
+                </Stack>
+            </div>
+            
+            <Footer name="Andre G. Enriquez" course="BSCS" section="F2"/>
         </div>
     );
   }
